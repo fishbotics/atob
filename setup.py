ef031a6fb50db68f344bc13bf01252dc72e240d4
Adam Fishman <afishman@cs.washington.edu>
2021-07-28 16:50:45 -0700

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='atob',
+    version='0.0.1',
+    packages=['atob'],
+    install_requires=[
+        # 'pybullet',
+	# 'numpy',
+	# 'pyquaternion',
+    ],
+)
