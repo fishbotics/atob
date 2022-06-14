55ae9e3dc842a80a75cd5757a7fa3f38d2a9fc67
Adam Fishman <afishman@cs.washington.edu>
2022-06-14 09:41:56 -0700

    Updated example file

--- atob/example.py
+++ atob/example.py
@@ -1,30 +1,43 @@
-from atob.planner import FrankaRRTConnectPlanner
+from atob.planner import FrankaAITStarPlanner
 from geometrout.primitive import Cuboid
-from robofin.robots import FrankaRobot
+from robofin.robots import FrankaRobot, FrankaRealRobot
 from robofin.bullet import Bullet
+from robofin.collision import FrankaSelfCollisionChecker
 import time
+import numpy as np
+
+
+def gen_valid_config(sim, robot, selfcc):
+    while True:
+        q = FrankaRealRobot.random_configuration()
+        robot.marionette(q)
+        if not sim.in_collision(
+            robot, check_self=True
+        ) and not selfcc.has_self_collision(q):
+            break
+    return q
 
 
 def example1():
-    start = [
-        -0.53556,
-        -0.13557598,
-        2.06197249,
-        -2.07799268,
-        0.25589343,
-        1.91628114,
-        1.45641799,
-    ]
-    goal = [
-        -0.89444609,
-        0.0053469,
-        -1.21969162,
-        -2.35934311,
-        0.56881887,
-        1.73707844,
-        2.30240395,
-    ]
-    planner = FrankaRRTConnectPlanner()
+    # start = [
+    #     -0.53556,
+    #     -0.13557598,
+    #     2.06197249,
+    #     -2.07799268,
+    #     0.25589343,
+    #     1.91628114,
+    #     1.45641799,
+    # ]
+    # goal = [
+    #     -0.89444609,
+    #     0.0053469,
+    #     -1.21969162,
+    #     -2.35934311,
+    #     0.56881887,
+    #     1.73707844,
+    #     2.30240395,
+    # ]
+    planner = FrankaAITStarPlanner()
 
     # You can turn the gui off to make this faster/disable visuals
     sim = Bullet(gui=True)
@@ -38,8 +51,19 @@ def example1():
         ]
     )
     franka_arm = sim.load_robot(FrankaRobot)
+    selfcc = FrankaSelfCollisionChecker()
+    start = gen_valid_config(sim, franka_arm, selfcc)
+    goal = gen_valid_config(sim, franka_arm, selfcc)
+
+    planner.load_self_collision_checker(selfcc)
     planner.load_simulation(sim, franka_arm)
-    path = planner.plan(start=start, goal=goal)
+    path = planner.plan(
+        start=start, goal=goal, min_solution_time=15, max_runtime=20, verbose=True
+    )
+    assert np.allclose(path[0], start)
+    assert np.allclose(path[-1], goal)
+    path = planner.smooth(path, timesteps=50)
+    print(type(path[0]))
 
     # This just visualizes the final path
     franka_arm.marionette(start)
