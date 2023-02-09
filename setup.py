644b96b3d9c6d73a89cee7616792b97c5c7f269f
Adam Fishman <afishman@cs.washington.edu>
2023-02-08 16:55:05 -0800

    Fixed issue in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="atob",
     version="0.0.2",
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=[
         # 'pybullet',
         # 'numpy',
