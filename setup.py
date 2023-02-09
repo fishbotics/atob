dcce637a4fedaacfbd85bbacd13808635a74f0a5
Adam Fishman <afishman@cs.washington.edu>
2023-02-08 16:51:48 -0800

    Updated version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='atob',
-    version='0.0.1',
-    packages=['atob'],
+    name="atob",
+    version="0.0.2",
+    packages=["atob"],
     install_requires=[
         # 'pybullet',
-	# 'numpy',
-	# 'pyquaternion',
+        # 'numpy',
+        # 'pyquaternion',
     ],
 )
